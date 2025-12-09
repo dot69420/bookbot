eef3eba369f058947cf6185557448996baa7ea91
dot69420 <meucarojoao@outlook.com.br>
2025-12-09 18:27:37 -0300

    Functions added

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+      path_to_book = "books/frankenstein.txt"
+      book_text = get_book_text(path_to_book)
+      words_num = get_num_words(book_text)
+      print(f"Found {words_num} total words")
+
+def get_book_text(file_path):
+        with open(file_path) as f:
+            book_text = f.read()
+        return book_text
+
+def get_num_words(book_text):
+  words = book_text.split()
+  words_num = len(words)
+  return words_num
+
+main()
